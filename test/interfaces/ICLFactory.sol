8c7a37c36e3122ce29029452b1699191d275743b
clement-ux <clemmoller@gmail.com>
2024-08-02 13:30:17 +0200

    feat: import library and interfaces.

---
+++ test/interfaces/ICLFactory.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.4;
+
+interface ICLFactory {
+    event DefaultUnstakedFeeChanged(uint24 indexed oldUnstakedFee, uint24 indexed newUnstakedFee);
+    event OwnerChanged(address indexed oldOwner, address indexed newOwner);
+    event PoolCreated(address indexed token0, address indexed token1, int24 indexed tickSpacing, address pool);
+    event SwapFeeManagerChanged(address indexed oldFeeManager, address indexed newFeeManager);
+    event SwapFeeModuleChanged(address indexed oldFeeModule, address indexed newFeeModule);
+    event TickSpacingEnabled(int24 indexed tickSpacing, uint24 indexed fee);
+    event UnstakedFeeManagerChanged(address indexed oldFeeManager, address indexed newFeeManager);
+    event UnstakedFeeModuleChanged(address indexed oldFeeModule, address indexed newFeeModule);
+
+    function allPools(uint256) external view returns (address);
+    function allPoolsLength() external view returns (uint256);
+    function createPool(address tokenA, address tokenB, int24 tickSpacing, uint160 sqrtPriceX96)
+        external
+        returns (address pool);
+    function defaultUnstakedFee() external view returns (uint24);
+    function enableTickSpacing(int24 tickSpacing, uint24 fee) external;
+    function factoryRegistry() external view returns (address);
+    function getPool(address, address, int24) external view returns (address);
+    function getSwapFee(address pool) external view returns (uint24);
+    function getUnstakedFee(address pool) external view returns (uint24);
+    function isPool(address pool) external view returns (bool);
+    function owner() external view returns (address);
+    function poolImplementation() external view returns (address);
+    function setDefaultUnstakedFee(uint24 _defaultUnstakedFee) external;
+    function setOwner(address _owner) external;
+    function setSwapFeeManager(address _swapFeeManager) external;
+    function setSwapFeeModule(address _swapFeeModule) external;
+    function setUnstakedFeeManager(address _unstakedFeeManager) external;
+    function setUnstakedFeeModule(address _unstakedFeeModule) external;
+    function swapFeeManager() external view returns (address);
+    function swapFeeModule() external view returns (address);
+    function tickSpacingToFee(int24) external view returns (uint24);
+    function tickSpacings() external view returns (int24[] memory);
+    function unstakedFeeManager() external view returns (address);
+    function unstakedFeeModule() external view returns (address);
+    function voter() external view returns (address);
+}
---
+++ test/utils/Addresses.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.25;
+
+library Base {
+    address public constant WETH = 0x4200000000000000000000000000000000000006;
+    address public constant CLFACTORY = 0x9592CD9B267748cbfBDe90Ac9F7DF3c437A6d51B;
+    address public constant CLPOOL_IMPL = 0xF926b5acC092E396A3b337642Be2E4cAe3f5da8E;
+    address public constant NFT_POSITION_MANAGER = 0xc741beb2156827704A1466575ccA1cBf726a1178;
+}
