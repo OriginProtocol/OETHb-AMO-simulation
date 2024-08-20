664f7ab08f86f533cb43dc2b3c351bedf1487193
clement-ux <clemmoller@gmail.com>
2024-08-20 15:50:23 +0200

    feat: AMO and Pool actions.

---
+++ test/AMOActions.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.25;
+
+// Base test
+import {Base_Test_} from "test/Base.sol";
+
+contract Base_AMO_Actions_ is Base_Test_ {
+    function allocate() public {
+        vault.allocate();
+    }
+
+    function rebalance(uint256 _amountToSwap, uint256 _minTokenReceived, bool _swapWeth) public {
+        vm.prank(strategy.governor());
+        strategy.rebalance(_amountToSwap, _minTokenReceived, _swapWeth);
+    }
+
+    function allocateAndRebalance(uint256 _amountToSwap, uint256 _minTokenReceived, bool _swapWeth) public {
+        allocate();
+        rebalance(_amountToSwap, _minTokenReceived, _swapWeth);
+    }
+}
---
+++ test/interfaces/IAMOStrategy.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.25;
+
+import {ICLPool} from "test/interfaces/ICLPool.sol";
+import {ISwapRouter} from "test/interfaces/ISwapRouter.sol";
+
+interface IAMOStrategy {
+    function governor() external view returns (address);
+    function rebalance(uint256 _amountToSwap, uint256 _minTokenReceived, bool _swapWeth) external;
+    function clPool() external view returns (ICLPool);
+    function vaultAddress() external view returns (address);
+    function swapRouter() external view returns (ISwapRouter);
+}
