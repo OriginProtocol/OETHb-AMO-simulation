49eb352575ce013b60e1df2abcf3eda90e6c9a4f
clement-ux <clemmoller@gmail.com>
2024-08-21 15:31:20 +0200

    WIP

--- test/simulations/BaseSimulation.sol
+++ test/simulations/BaseSimulation.sol
@@ -3,6 +3,8 @@ pragma solidity 0.8.25;
 
 import {console} from "lib/forge-std/src/console.sol";
 
+import {SafeCastLib} from "@solady/utils/SafeCastLib.sol";
+
 import {TickMath} from "test/libraries/TickMath.sol";
 
 import {Base_AMO_Actions_} from "test/AMOActions.sol";
@@ -11,50 +13,27 @@ import {Base_Pool_Actions_} from "test/PoolActions.sol";
 import {BinarySearchQuoter} from "test/utils/BinarySearchQuoter.sol";
 
 contract Base_Simulations_ is Base_AMO_Actions_, Base_Pool_Actions_ {
+    using SafeCastLib for uint256;
+
+    ////////////////////////////////////////////////////////////////
+    /// --- CONSTANTS & IMMUTABLES
+    ////////////////////////////////////////////////////////////////
+    uint256 public constant DEFAULT_VARIANCE = 0; // 1e27 == 100%
+    uint256 public constant DEFAULT_MAX_ITERATIONS = 500;
+    uint160 public immutable DEFAULT_PRICE_LIMITE_LOW = TickMath.getSqrtRatioAtTick(-10_000);
+    uint160 public immutable DEFAULT_PRICE_LIMITE_HIGH = TickMath.getSqrtRatioAtTick(10_000);
+    uint256 public constant DEFAULT_AMOUNT_TO_SWAP_START_MIN = 0.000001 ether;
+    uint256 public constant DEFAULT_AMOUNT_TO_SWAP_START_MAX = 100000 ether;
+
     function test_Quoter_WETHToOETHb() public {
         // Someone mint 100 OETHb
         deal(address(oethb), address(this), 100 ether);
 
         // Deposit 100 WETH and 100 OETHb between -10 and 10
-        provideLiquidity(100 ether, 100 ether, -10, 10, true);
+        //provideLiquidity(100 ether, 100 ether, -10, 10, true);
 
         // We allocate WETH to strategy
-        allocate();
-
-        // How much WETH we need to swap to reach target price
-        (uint160 currentPrice,,,,,) = pool.slot0();
-        uint160 targetPrice = TickMath.getSqrtRatioAtTick(-10);
-        emit log_named_uint("Current price: ", currentPrice);
-        emit log_named_uint("Target  price: ", targetPrice);
-
-        uint160 priceLimit = TickMath.getSqrtRatioAtTick(-100);
-        uint256 minAmount = 0.000001 ether;
-        uint256 maxAmount = 1000000 ether;
-        uint256 variance = 0;
-
-        (uint256 amountToSwap, uint160 priceAfter, uint256 iterations) = BinarySearchQuoter.amountToSwapToReachPrice(
-            BinarySearchQuoter.BinarySearchQuoterParams({
-                swapWETHForOETHB: true,
-                targetPrice: targetPrice,
-                sqrtPriceLimitX96: priceLimit,
-                minAmount: minAmount,
-                maxAmount: maxAmount,
-                allowedVariance: variance,
-                maxIterations: 100
-            })
-        );
-        uint256 deviance = (max(targetPrice, priceAfter) - min(targetPrice, priceAfter)) * 1e18 / targetPrice;
-
-        console.log("TargetPrice: ", targetPrice);
-        console.log("PriceAfter : ", priceAfter);
-        console.log("deviance: %18e", deviance);
-        console.log("Amount to swap: %18e", amountToSwap);
-        console.log("Iterations: ", iterations);
-
-        swapWETHExactInput(amountToSwap, priceLimit, true);
-        (currentPrice,,,,,) = pool.slot0();
-        console.log("Current price after swap: ", currentPrice);
-        console.log("Targeted price:           ", targetPrice);
+        allocateAndRebalance(amountOfWETHToSwapToReachPrice(true), 0, true);
     }
 
     function test_Quoter_OETHbToWETH() public {
@@ -101,6 +80,72 @@ contract Base_Simulations_ is Base_AMO_Actions_, Base_Pool_Actions_ {
         (currentPrice,,,,,) = pool.slot0();
         console.log("Current price after swap: ", currentPrice);
         console.log("Targeted price:           ", targetPrice);
+    }
+
+    function amountOfWETHToSwapToReachPrice(bool display) public returns (uint256) {
+        (uint160 priceBefore,,,,,) = pool.slot0();
+        uint160 targetPrice = getTargetPrice();
+
+        (uint256 amountToSwap, uint160 priceAfter, uint256 iterations) = BinarySearchQuoter.amountToSwapToReachPrice(
+            BinarySearchQuoter.BinarySearchQuoterParams({
+                swapWETHForOETHB: true,
+                targetPrice: targetPrice,
+                sqrtPriceLimitX96: DEFAULT_PRICE_LIMITE_LOW,
+                minAmount: DEFAULT_AMOUNT_TO_SWAP_START_MIN,
+                maxAmount: DEFAULT_AMOUNT_TO_SWAP_START_MAX,
+                allowedVariance: DEFAULT_VARIANCE,
+                maxIterations: DEFAULT_MAX_ITERATIONS
+            })
+        );
+
+        if (display) {
+            uint256 deviance = (max(targetPrice, priceAfter) - min(targetPrice, priceAfter)) * 1e18 / targetPrice;
+            console.log("PriceBefore: ", priceBefore);
+            console.log("TargetPrice: ", targetPrice);
+            console.log("PriceAfter : ", priceAfter);
+            console.log("deviance: %18e", deviance);
+            console.log("Amount to swap: %18e", amountToSwap);
+            console.log("Iterations: ", iterations);
+        }
+
+        return amountToSwap;
+    }
+
+    function amountOfOETHbToSwapToReachPrice(bool display) public returns (uint256) {
+        (uint160 priceBefore,,,,,) = pool.slot0();
+        uint160 targetPrice = getTargetPrice();
+
+        (uint256 amountToSwap, uint160 priceAfter, uint256 iterations) = BinarySearchQuoter.amountToSwapToReachPrice(
+            BinarySearchQuoter.BinarySearchQuoterParams({
+                swapWETHForOETHB: false,
+                targetPrice: targetPrice,
+                sqrtPriceLimitX96: DEFAULT_PRICE_LIMITE_HIGH,
+                minAmount: DEFAULT_AMOUNT_TO_SWAP_START_MIN,
+                maxAmount: DEFAULT_AMOUNT_TO_SWAP_START_MAX,
+                allowedVariance: DEFAULT_VARIANCE,
+                maxIterations: DEFAULT_MAX_ITERATIONS
+            })
+        );
+
+        if (display) {
+            uint256 deviance = (max(targetPrice, priceAfter) - min(targetPrice, priceAfter)) * 1e18 / targetPrice;
+            console.log("PriceBefore: ", priceBefore);
+            console.log("TargetPrice: ", targetPrice);
+            console.log("PriceAfter : ", priceAfter);
+            console.log("deviance: %18e", deviance);
+            console.log("Amount to swap: %18e", amountToSwap);
+            console.log("Iterations: ", iterations);
+        }
+
+        return amountToSwap;
+    }
+
+    function getTargetPrice() public view returns (uint160) {
+        uint256 share = strategy.poolWethShare();
+        return (
+            TickMath.getSqrtRatioAtTick(-1) * share.toUint160()
+                + TickMath.getSqrtRatioAtTick(0) * (1e18 - share).toUint160()
+        ) / 1e18;
     }
 
     function min(uint256 a, uint256 b) internal pure returns (uint256) {
