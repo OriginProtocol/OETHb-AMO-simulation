ab07547ae655f3d12fe3148fb6fc10f4e217b3ba
clement-ux <clemmoller@gmail.com>
2024-08-22 23:41:59 +0200

    fix: remove useless tests.

--- test/simulations/BaseSimulation.sol
+++ test/simulations/BaseSimulation.sol
@@ -26,63 +26,6 @@ contract Base_Simulations_ is Base_AMO_Actions_, Base_Pool_Actions_ {
     uint256 public constant DEFAULT_AMOUNT_TO_SWAP_START_MAX = 100000 ether;
     uint256 public constant DEFAULT_INITIAL_DEPOSIT = 40 ether;
 
-    function test_Quoter_WETHToOETHb() public {
-        // Someone mint 100 OETHb
-        deal(address(oethb), address(this), 100 ether);
-
-        // Deposit 100 WETH and 100 OETHb between -10 and 10
-        //provideLiquidity(100 ether, 100 ether, -10, 10, true);
-
-        // We allocate WETH to strategy
-        allocateAndRebalance(amountOfWETHToSwapToReachPrice(true), 0, true);
-    }
-
-    function test_Quoter_OETHbToWETH() public {
-        // Someone mint 100 OETHb
-        deal(address(oethb), address(this), 100 ether);
-
-        // Deposit 100 WETH and 100 OETHb between -10 and 10
-        provideLiquidity(100 ether, 100 ether, -10, 10, true);
-
-        // We allocate WETH to strategy
-        allocate();
-
-        // How much WETH we need to swap to reach target price
-        (uint160 currentPrice,,,,,) = pool.slot0();
-        uint160 targetPrice = TickMath.getSqrtRatioAtTick(10);
-        emit log_named_uint("Current price: ", currentPrice);
-        emit log_named_uint("Target  price: ", targetPrice);
-
-        uint160 priceLimit = TickMath.getSqrtRatioAtTick(100);
-        uint256 minAmount = 0.000001 ether;
-        uint256 maxAmount = 1000000 ether;
-        uint256 variance = 0;
-
-        (uint256 amountToSwap, uint160 priceAfter, uint256 iterations) = BinarySearchQuoter.amountToSwapToReachPrice(
-            BinarySearchQuoter.BinarySearchQuoterParams({
-                swapWETHForOETHB: false,
-                targetPrice: targetPrice,
-                sqrtPriceLimitX96: priceLimit,
-                minAmount: minAmount,
-                maxAmount: maxAmount,
-                allowedVariance: variance,
-                maxIterations: 100
-            })
-        );
-        uint256 deviance = (max(targetPrice, priceAfter) - min(targetPrice, priceAfter)) * 1e18 / targetPrice;
-
-        console.log("TargetPrice: ", targetPrice);
-        console.log("PriceAfter : ", priceAfter);
-        console.log("deviance: %18e", deviance);
-        console.log("Amount to swap: %18e", amountToSwap);
-        console.log("Iterations: ", iterations);
-
-        swapOETHbExactInput(amountToSwap, priceLimit, true);
-        (currentPrice,,,,,) = pool.slot0();
-        console.log("Current price after swap: ", currentPrice);
-        console.log("Targeted price:           ", targetPrice);
-    }
-
     function amountOfWETHToSwapToReachPrice(bool display) public returns (uint256) {
         (uint160 priceBefore,,,,,) = pool.slot0();
         uint160 targetPrice = getTargetPrice();
@@ -159,7 +102,7 @@ contract Base_Simulations_ is Base_AMO_Actions_, Base_Pool_Actions_ {
             })
         );
         // Revert to the previous state
-        vm.revertToAndDelete(id);
+        require(vm.revertToAndDelete(id), "RevertToAndDelete failed");
 
         console.log("Amount to swap: %18e", amount);
         console.log("Iterations: ", iterations);
@@ -180,12 +123,12 @@ contract Base_Simulations_ is Base_AMO_Actions_, Base_Pool_Actions_ {
                 minAmount: DEFAULT_AMOUNT_TO_SWAP_START_MIN,
                 maxAmount: DEFAULT_AMOUNT_TO_SWAP_START_MAX,
                 allowedVariance: 0, // Not needed here
-                maxIterations: 20
+                maxIterations: 50
             })
         );
 
         // Revert to the previous state
-        vm.revertToAndDelete(id);
+        require(vm.revertToAndDelete(id), "RevertToAndDelete failed");
         console.log("Amount to swap: %18e", amount);
         console.log("Iterations: ", iterations);
         return (amount);
